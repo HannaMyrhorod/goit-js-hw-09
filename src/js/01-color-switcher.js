94239bcd360094a53b9739874b11c2414c64d081
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-06-24 14:25:41 +0300

    Add task #1 Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.disabled = true;
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor(); 
+     }, 1000)
+};
+
+function onStopBtnClick() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    clearInterval(timerId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
